b3c45f8e02eb7df9340ae6b05017f96e49e5ea9a
Kamalov <kfarrux2005@gmail.com>
2024-07-17 02:17:07 +0500

    Cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import userRouter from "./user/userRouter";
+import cors from 'cors';
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import bookRouter from "./book/bookRouter";
 
 const app = express();
 
+app.use(cors('*'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
